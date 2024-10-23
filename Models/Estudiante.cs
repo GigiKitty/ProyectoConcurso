cb77d0517d9264c4bdfce0fb62b8751532fe0cd2
GigiKitty <alelysainz938@gmail.com>
2024-10-23 10:41:51 -0700

    se creo la clase estudiantes con todos los campos

---
+++ Models/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcursoPogramacion2024pro.Models
+{
+	public class Estudiante
+	{
+		[Key]
+
+		public int IdEstudiante { get; set; }
+		[Required(ErrorMessage = "Campo requerido!")]
+
+		String NombreEstudiante { get; set; }
+		[Required (ErrorMessage ="Campo requerido!")]
+
+		String ApellidoEstudiante { get; set; }
+		[Required (ErrorMessage ="Campo requerido!")]
+
+		DateTime FechaNacimiento { get; set; }
+		[Required(ErrorMessage ="Campo requerido!")]
+
+	}
+}
